11b0d48ffcf5fbae0e33ae5ef09e742ec4160749
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-04-05 00:51:41 +0700

    Client_penjualan
    menghapus fungsi yang tidak diperlukan

--- soal2/soal3_cl2.c
+++ soal2/soal3_cl2.c
@@ -18,21 +18,12 @@ int main(int argc, char const *argv[]) {
         printf("\n Socket creation error \n");
         return -1;
     }
-   // if ((sock1 = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-//	printf("\n Socket creation error \n");
- //       return -1;
-  //  }
 
     memset(&serv_addr, '0', sizeof(serv_addr));
-    //memset(&serv_addr1, '0', sizeof(serv_addr1));
-
+   
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;//inet_addr("192.168.1.2");
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(PORT);
-
-//    serv_addr1.sin_family = AF_INET;
- //   serv_addr1.sin_addr.s_addr = INADDR_ANY;inet_addr("192.168.1.3");
-  //  serv_addr1.sin_port = htons(PORT);
 
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
         printf("\nInvalid address/ Address not supported \n");
@@ -44,23 +35,16 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
     
-   //if (connect(sock, (struct sockaddr *)&serv_addr1, sizeof(serv_addr1)) < 0) {
-  //     printf("\nConnection Failed \n");
-   //     return -1;
-    //}
-
     while(1){
 
 	    fgets(permintaan, 100, stdin);
-	   // send(sock , permintaan , strlen(permintaan) , 0 );
+	 
 	    if(strcmp(permintaan,beli)){
             	send(sock , permintaan , strlen(permintaan) , 0 );
 	    	valread = read( sock , buffer, 1024);
 	    	printf("%s\n",buffer );
 	    }
-	 //  else{
-	  //  	send(sock1 , permintaan , strlen(permintaan) , 0 );
-	   //}
+	
     }
 
     return 0;
