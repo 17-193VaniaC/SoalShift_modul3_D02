61d8add9ca043c70a4dbd3de71d4d1e7ae4b3dd1
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-04-05 00:47:48 +0700

    Hapus fungsi yang tidak diperlukan

--- soal2/soal3_pb.c
+++ soal2/soal3_pb.c
@@ -24,7 +24,7 @@ int main(int argc, char const *argv[])
 {	//thread
 	pthread_t thread1, thread2;
 	int iret1, iret2;
-//	char *tambah="tambah";
+
 	//shared mem
         key_t key = 1234;
         int *value;
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[])
 
 	//stok awal ada 10
         *value = 10;
-	printf("%d \n",*value);
+
 	//thread 1 untuk nerima pesan client
 	iret1 = pthread_create( &thread1, NULL, get_message, (void *) value);
 	if(iret1)
@@ -44,20 +44,9 @@ int main(int argc, char const *argv[])
         	exit(EXIT_FAILURE);
         }
 
-	//thread 2 untuk menampilkan stock saat ini
-   //     iret2 = pthread_create( &thread2, NULL, print_stock, (void *) value);
-    //    if(iret2)
-     //   {
-      // 		fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
-       //         exit(EXIT_FAILURE);
-       // }
-
-	printf("valuenya %ls\n", value);
-    printf("pthread_create() for thread 1 returns: %d\n",iret1);
-   // printf("pthread_create() for thread 2 returns: %d\n",iret2);
-
+	printf("pthread_create() for thread 1 returns: %d\n",iret1);
+  
     pthread_join( thread1, NULL);
- //   pthread_join( thread2, NULL); 
 
     exit(EXIT_SUCCESS);
 
@@ -72,35 +61,29 @@ void *get_message( void *ptr){
         int opt=1;
         int addrlen = sizeof(address);
         char buffer[1024] = {0};
-	printf("ini thread1\n");
-
+	
         if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
                 perror("socket failed");
                 exit(EXIT_FAILURE);
          }
-	printf("socket success\n");
-
+	
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
                 perror("setsockopt");
                 exit(EXIT_FAILURE);
          } 
-	printf("setsocket succes\n");
 
         address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;//inet_addr("192.168.1.1");
+        address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons( PORT );
 
         if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
                 perror("bind failed");
                 exit(EXIT_FAILURE);
         }
-	printf("bind succes\n");
-
         if (listen(server_fd, 1) < 0) {//listen cuma ke 1 client
                 perror("listen");
                 exit(EXIT_FAILURE);
         }
-	printf("listen\n");
 
 	 if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
                 perror("accept");
@@ -111,9 +94,7 @@ void *get_message( void *ptr){
 	tambahin = (int *) ptr;
 	char *bisa="transaksi berhasil\n";
 	char *gbisa="transaksi gagal\n";
-	printf("nahini\n");
 	while(1){
-		printf("ini-thread-1\n");
 		valread = read( new_socket , buffer, 1024);
 		if(strcmp(buffer,beli)){
 			if(*tambahin>0){
