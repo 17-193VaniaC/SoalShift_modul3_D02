cbba03a56279cc07577c58fd43aaedb0cb3242fb
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-04-05 00:25:03 +0700

    Belum dirapihin

---
+++ soal2/soal3_cl1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define PORT 8080
+  
+int main(int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    int sock = 0, valread, sock1;
+    struct sockaddr_in serv_addr, serv_addr1;
+    char buffer[1024] = {0};
+    char permintaan[100];
+    char *beli="tambah";
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+   // if ((sock1 = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+//	printf("\n Socket creation error \n");
+ //       return -1;
+  //  }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    //memset(&serv_addr1, '0', sizeof(serv_addr1));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;//inet_addr("192.168.1.2");
+    serv_addr.sin_port = htons(PORT);
+
+//    serv_addr1.sin_family = AF_INET;
+ //   serv_addr1.sin_addr.s_addr = INADDR_ANY;inet_addr("192.168.1.3");
+  //  serv_addr1.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    
+   //if (connect(sock, (struct sockaddr *)&serv_addr1, sizeof(serv_addr1)) < 0) {
+  //     printf("\nConnection Failed \n");
+   //     return -1;
+    //}
+
+    while(1){
+
+	    fgets(permintaan, 100, stdin);
+	   // send(sock , permintaan , strlen(permintaan) , 0 );
+	    if(strcmp(permintaan,beli)){
+            	send(sock , permintaan , strlen(permintaan) , 0 );
+	    	//valread = read( sock , buffer, 1024);
+	    	//printf("%s\n",buffer );
+	    }
+	 //  else{
+	  //  	send(sock1 , permintaan , strlen(permintaan) , 0 );
+	   //}
+    }
+
+    return 0;
+}
---
+++ soal2/soal3_pb.c
@@ -0,0 +1,129 @@
+//thread
+#include <stdlib.h>
+#include <pthread.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+//shared mem
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+//server
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define PORT 8080
+
+
+void *get_message( void * ptr);
+void *print_stock( void * banyak);
+
+int main(int argc, char const *argv[])
+{	//thread
+	pthread_t thread1, thread2;
+	int iret1, iret2;
+//	char *tambah="tambah";
+	//shared mem
+        key_t key = 1234;
+        int *value;
+
+	//inisialisasi shared mem
+        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+        value = shmat(shmid, NULL, 0);
+
+	//stok awal ada 10
+        *value = 10;
+	printf("%d \n",*value);
+	//thread 1 untuk nerima pesan client
+	iret1 = pthread_create( &thread1, NULL, get_message, (void *) value);
+	if(iret1)
+        {
+		fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
+        	exit(EXIT_FAILURE);
+        }
+
+	//thread 2 untuk menampilkan stock saat ini
+   //     iret2 = pthread_create( &thread2, NULL, print_stock, (void *) value);
+    //    if(iret2)
+     //   {
+      // 		fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+       //         exit(EXIT_FAILURE);
+       // }
+
+	printf("valuenya %ls\n", value);
+    printf("pthread_create() for thread 1 returns: %d\n",iret1);
+   // printf("pthread_create() for thread 2 returns: %d\n",iret2);
+
+    pthread_join( thread1, NULL);
+ //   pthread_join( thread2, NULL); 
+
+    exit(EXIT_SUCCESS);
+
+}
+
+
+void *get_message( void *ptr){
+
+        char *beli="beli";
+	int server_fd, new_socket, valread;
+        struct sockaddr_in address;
+        int opt=1;
+        int addrlen = sizeof(address);
+        char buffer[1024] = {0};
+	printf("ini thread1\n");
+
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+                perror("socket failed");
+                exit(EXIT_FAILURE);
+         }
+	printf("socket success\n");
+
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+                perror("setsockopt");
+                exit(EXIT_FAILURE);
+         } 
+	printf("setsocket succes\n");
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;//inet_addr("192.168.1.1");
+        address.sin_port = htons( PORT );
+
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+                perror("bind failed");
+                exit(EXIT_FAILURE);
+        }
+	printf("bind succes\n");
+
+        if (listen(server_fd, 1) < 0) {//listen cuma ke 1 client
+                perror("listen");
+                exit(EXIT_FAILURE);
+        }
+	printf("listen\n");
+
+	 if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+        }
+
+	int *tambahin;
+	tambahin = (int *) ptr;
+	char *bisa="transaksi berhasil\n";
+	char *gbisa="transaksi gagal\n";
+	printf("nahini\n");
+	while(1){
+		printf("ini-thread-1\n");
+		valread = read( new_socket , buffer, 1024);
+		if(strcmp(buffer,beli)){
+			if(*tambahin>0){
+			send(new_socket, bisa, strlen(bisa), 0);
+			*tambahin=*tambahin-1;
+			}
+			else{
+			send(new_socket, gbisa, strlen(gbisa), 0);
+			}
+		}
+		
+	}
+}
