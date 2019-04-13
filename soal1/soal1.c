2302096045744e1e8ac37b3f6cb6a0be5c8aef62
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-04-13 20:37:39 +0700

    Revisi penambahan mutual exclusion
    Dengan perubahan struktur data

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -3,19 +3,28 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdint.h>
+int status;
+
+typedef struct faktorialz{
+int urut;
+int isi;
+//int hasil;
+}Lala;
+
 
 void *faktorial(void* nilai)
 {   
-    int fak_loop = (int) (intptr_t)nilai;
+    Lala * fak_loop = (Lala* )nilai;
     int hasil = (int) (intptr_t)nilai;
     int j;
-
-    for (j=1; j<fak_loop; j++)
+    while(status<fak_loop->urut){}
+    for (j=1; j<fak_loop->isi; j++)
     {
         hasil = hasil * j;
     }
-
-    printf("%d! = %d\n", fak_loop, hasil);
+    printf("thread id:\t%lu\n",pthread_self());
+    printf("%d! = %d\n", fak_loop->isi, hasil);
+    status++;
 }
 
 
@@ -23,35 +32,45 @@ int main(int argc, char *array[])
 {
     int j,i,a, arr[100];
     int *de;
-
+    Lala huha[argc];
     for (i = 1; i < argc; i++)
     {
 	int mbuh = atoi(array[i]);
-	arr[i] = mbuh;
+	huha[i].isi = mbuh;
     }
 
- for (i=1; i<argc; ++i)
+for (i=1; i<argc; ++i)
        {
            for (j= i+1; j<argc; ++j)
            {
-               if (arr[i] > arr[j])
+               if (huha[i].isi > huha[j].isi)
                {
-                   int a =  arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = a;
+                   int a =  huha[i].isi;
+                   huha[i].isi = huha[j].isi;
+                   huha[j].isi = a;
                }
            }
-       }   
-
-    int cr;
-    pthread_t tid;
+       }
 
     for (i = 1; i < argc; i++)
+    {
+        huha[i].urut = i;
+    }
+
+    status=1;
+    int cr;
+    pthread_t tid[argc];
+    for (i = 1; i < argc; i++)
 	{
-
-	    cr = pthread_create(&(tid), NULL, &faktorial, (void *) (intptr_t) arr[i]);
-	    if(cr!=0) printf("\nPembuatan thread error\n");
-	    pthread_join(tid, NULL);
+	   cr=pthread_create(&(tid[i]), NULL, &faktorial, (void *) &huha[i]);
 	}
-}
 
+	for (i = 1; i < argc; i++){
+
+	    pthread_join(tid[i], NULL);//loopnya harusnya beda
+	}/*
+for(i=0;i<argc;i++){
+       status=arr[i];
+}
+*/
+}
